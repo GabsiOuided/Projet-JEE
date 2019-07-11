b66477eb71d9c4a2d69bef9d914c9c6db8228f6d
Gabsi <gabsiwided12@gmail.Com>
2019-07-11 09:28:27 +0200

    Examen JEE
    Gabsi Ouided Projet

---
+++ src/main/java/projectJEE/demo/GabsiOuidedApplication.java
@@ -0,0 +1,13 @@
+package projectJEE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabsiOuidedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabsiOuidedApplication.class, args);
+	}
+
+}
